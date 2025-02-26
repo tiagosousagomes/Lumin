3151a94b9b7b068277ac03dc5044d701b627d057
Felipe Renan <felipeferreira3146@gmail.com>
2025-02-26 12:24:37 -0300

    Criando createPost
    Criando a função para criar posts

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,8 +1,46 @@
+<<<<<<< Updated upstream
 const Post = require('../models/post');
 const User = require('../models/user');
 
     exports.createPost = async (req, res) => {
         const {title, text, userId} = req.body;
+=======
+const Post = require("../models/post");
+const User = require("../models/post")
+
+const createPost = async (req, res) => {
+    try{
+        const {content, author} = req.body;
+
+        const user = await User.findById(author);
+        if(!user) {
+            return res.status(404).json({
+                sucess: false,
+                message: "Autor não encontrado"
+            })
+        }
+
+        const post = new Post({
+            content,
+            author,
+        })
+
+        await post.save();
+
+        res.status(201).json({
+            sucess: true,
+            message: "Post criado com sucesso",
+            data: post
+        })
+    } catch(err) {
+        res.status(500).json({
+            sucess: false,
+            message: "Erro ao criar o post",
+            error: err.message
+        })
+    }
+};
+>>>>>>> Stashed changes
 
             try { 
         const user = await User.findById(userId);
