34a2c1769f031d52f40ba4fb1bfd16a9c1b11825
Felipe Renan <felipeferreira3146@gmail.com>
2025-02-26 14:28:57 -0300

    Ajustando commit
    Ajustando commit da função para criar posts

---
+++ src/controllers/commentController.js
@@ -0,0 +1,19 @@
+const Comment = require("../models/comment");
+
+const createComment = async (req, res) => {
+    // Implementação para criar um comentário
+};
+
+const getCommentsByPost = async (req, res) => {
+    // Implementação para listar comentários de um post
+};
+
+const updateComment = async (req, res) => {
+    // Implementação para atualizar um comentário
+};
+
+const deleteComment = async (req, res) => {
+    // Implementação para deletar um comentário
+};
+
+module.exports = { createComment, getCommentsByPost, updateComment, deleteComment };
--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-const Post = require('../models/post');
-const User = require('../models/user');
-
-    exports.createPost = async (req, res) => {
-        const {title, text, userId} = req.body;
-=======
 const Post = require("../models/post");
 const User = require("../models/post")
 
@@ -39,46 +32,4 @@ const createPost = async (req, res) => {
             error: err.message
         })
     }
-};
->>>>>>> Stashed changes
-
-            try { 
-        const user = await User.findById(userId);
-            if (!user){
-            return res.status(400).json({ error: 'Usuário não encontrado!'})
-        }
-
-        const post = new Post({ title, text, user: userId });
-        await post.save();
-
-        user.posts.push(post._id);
-
-        res.status(201).json(post);
-    } catch(error){
-        console.error(error);
-
-        if(error.name === 'Erro de validção') {
-            return res.status(400).json({ message: 'Input inválido', error: error.message});
-        }
-        res.status(500).json({message: 'Erro de servirdor', error: error.message});
-    };
-
-
-    exports.getAllPosts = async(req, res) => {
-        try{
-            const posts = await Post.find().populate('user', 'name email')
-            res.status(200).json(posts);
-        } catch (error){
-            console.error(error);
-            res.status(500).json({message: 'Erro de servirdor', error: error.message});
-        }
-    };
-
-    exports.deletePost = async(req,res) => {
-        try{ 
-    
-        } catch(error){
-
-        }
-    };
 };
