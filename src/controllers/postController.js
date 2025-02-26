eb0daf09cb286a843ba379ac69229c77b7e1aa5e
Felipe Renan <felipeferreira3146@gmail.com>
2025-02-26 12:06:45 -0300

    Criando Controllers
    Gerado os controllers para todos os models, ainda sem implementação

---
+++ src/controllers/postController.js
@@ -0,0 +1,23 @@
+const Post = require("../models/post");
+
+const createPost = async (req, res) => {
+    // Implementação para criar um post
+};
+
+const getAllPosts = async (req, res) => {
+    // Implementação para listar todos os posts
+};
+
+const getPostById = async (req, res) => {
+    // Implementação para obter um post por ID
+};
+
+const updatePost = async (req, res) => {
+    // Implementação para atualizar um post
+};
+
+const deletePost = async (req, res) => {
+    // Implementação para deletar um post
+};
+
+module.exports = { createPost, getAllPosts, getPostById, updatePost, deletePost };
