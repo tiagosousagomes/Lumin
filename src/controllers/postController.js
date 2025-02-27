6f578bcc0b1faf4799e1086e10006f6e0c80a06c
Felipe Renan <felipeferreira3146@gmail.com>
2025-02-27 10:26:20 -0300

    Reinstalando dependências
    Alguns módulos, como o Mongoose estavam corrompidos

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,11 +1,47 @@
-const Post = require("../models/post");
+const Post = require("../models/user")
 
 const createPost = async (req, res) => {
-    // Implementação para criar um post
-};
+    try {
+      const { content, author } = req.body;
+  
+      // Verifica se o autor existe
+      const user = await User.findById(author);
+      if (!user) {
+        return res.status(404).json({
+          success: false,
+          message: "Autor não encontrado",
+        });
+      }
+  
+      // Cria o post
+      const post = new Post({
+        content,
+        author,
+      });
+  
+      // Salva o post no banco de dados
+      await post.save();
+  
+      res.status(201).json({
+        success: true,
+        message: "Post criado com sucesso",
+        data: post,
+      });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: "Erro ao criar o post",
+        error: err.message,
+      });
+    }
+  };
 
 const getAllPosts = async (req, res) => {
     // Implementação para listar todos os posts
+};
+
+const getAllPostFromUser = async (req, res) => {
+    //Implementação para obter todos os posts de um user
 };
 
 const getPostById = async (req, res) => {
@@ -20,4 +56,4 @@ const deletePost = async (req, res) => {
     // Implementação para deletar um post
 };
 
-module.exports = { createPost, getAllPosts, getPostById, updatePost, deletePost };
+export default { createPost, getAllPosts, getPostById, updatePost, deletePost };
