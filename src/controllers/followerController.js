dfe7c1ea5f5f7c9ddc7adeb011e2c50d7397cdd6
Felipe Renan <felipeferreira3146@gmail.com>
2025-03-11 15:37:27 -0300

    implementa followUser

--- src/controllers/followerController.js
+++ src/controllers/followerController.js
@@ -1,7 +1,46 @@
 const Follower = require("../models/follower");
+const User = require("../models/user");
 
 const followUser = async (req, res) => {
-    // Implementação para seguir um usuário
+    try{
+        const { followerID, followingID } = req.body;
+
+        const follower = await User.findById(followerID);
+        const following = await User.findById(followingID);
+
+        if (!follower || !following) {
+            return res.status(404).json({ 
+                succsess: false,
+                message: "Usuário não encontrado" });
+        }
+
+        const existingFollow = await Follower.findOne({ 
+            follower: followerID, 
+            following: followingID 
+        });
+
+        if (existingFollow) {
+            return res.status(400).json({ 
+                succsess: false,
+                message: "Usuário já está seguindo" });
+        }
+
+        const follow = new Follower({
+            follower: followerID,
+            following: followingID
+        });
+
+        await follow.save();
+
+        res.status(201).json({ 
+            succsess: true,
+            message: "Usuário seguido com sucesso" });
+    }catch(err){
+        res.status(500).json({ 
+            succsess: false,
+            message: "Erro ao seguir usuário" });
+    }
+    
 };
 
 const unfollowUser = async (req, res) => {
