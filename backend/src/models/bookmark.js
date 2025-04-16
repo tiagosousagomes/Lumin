35933ba6789ef96f892dc469d91bb63365dea38f
Felipe Renan <felipeferreira3146@gmail.com>
2025-04-16 17:17:57 -0300

    Cria model para bookmark

---
+++ backend/src/models/bookmark.js
@@ -0,0 +1,20 @@
+let mongoose = require("mongoose");
+
+let bookmarkSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+        required: true
+    }
+}, {
+    timestamp: true
+});
+
+bookmarkSchema.index({user: 1, post: 1}, {unique: true})
+
+module.exports = mongoose.model("bookmark", bookmarkSchema)
